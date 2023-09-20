13d81f66725f614ca84a519c646e74c109f6cdf5
kabita <kabita.texas@gmail.com>
2023-09-20 16:06:42 -0500

    chore: disabling jwt security stuff for testing purpose

--- src/main/java/com/example/employeeworld/jwtsecurity/repository/JWTUserRepository.java
+++ src/main/java/com/example/employeeworld/jwtsecurity/repository/JWTUserRepository.java
@@ -1,12 +1,12 @@
-package com.example.employeeworld.jwtsecurity.repository;
-
-import com.example.employeeworld.jwtsecurity.jwtmodel.User;
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-// TODO difference between extending CRUD repository and JPA repository
-public interface JWTUserRepository extends JpaRepository<User, Integer> {
-    Optional<User> findByEmail(String email);
-}
+//package com.example.employeeworld.jwtsecurity.repository;
+//
+//import com.example.employeeworld.jwtsecurity.jwtmodel.User;
+//import java.util.Optional;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//// TODO difference between extending CRUD repository and JPA repository
+//public interface JWTUserRepository extends JpaRepository<User, Integer> {
+//    Optional<User> findByEmail(String email);
+//}
--- src/main/java/com/example/employeeworld/jwtsecurity/service/AuthenticationService.java
+++ src/main/java/com/example/employeeworld/jwtsecurity/service/AuthenticationService.java
@@ -1,43 +1,43 @@
-package com.example.employeeworld.jwtsecurity.service;
-
-import com.example.employeeworld.jwtsecurity.jwtmodel.*;
-import com.example.employeeworld.jwtsecurity.repository.JWTUserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-/** Interact with database to register user */
-public class AuthenticationService {
-    private final JWTUserRepository repository;
-    private final PasswordEncoder passwordEncoder;
-    private final JWTService jwtService;
-    private final AuthenticationManager authenticationManager;
-
-    public AuthenticationResponse register(RegisterRequest request) {
-        var user =
-                User.builder()
-                        .firstname(request.getFirstname())
-                        .lastname(request.getLastname())
-                        .email(request.getEmail())
-                        .secret(passwordEncoder.encode(request.getPassword()))
-                        .role(Role.User)
-                        .build();
-        repository.save(user);
-        var jwtToken = jwtService.generateToken(user);
-
-        return AuthenticationResponse.builder().token(jwtToken).build();
-    }
-
-    public AuthenticationResponse authenticate(AuthenticationRequest request) {
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
-        var user = repository.findByEmail(request.getEmail()).orElseThrow();
-        var jwtToken = jwtService.generateToken(user);
-
-        return AuthenticationResponse.builder().token(jwtToken).build();
-    }
-}
+//package com.example.employeeworld.jwtsecurity.service;
+//
+//import com.example.employeeworld.jwtsecurity.jwtmodel.*;
+//import com.example.employeeworld.jwtsecurity.repository.JWTUserRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@RequiredArgsConstructor
+///** Interact with database to register user */
+//public class AuthenticationService {
+//    private final JWTUserRepository repository;
+//    private final PasswordEncoder passwordEncoder;
+//    private final JWTService jwtService;
+//    private final AuthenticationManager authenticationManager;
+//
+//    public AuthenticationResponse register(RegisterRequest request) {
+//        var user =
+//                User.builder()
+//                        .firstname(request.getFirstname())
+//                        .lastname(request.getLastname())
+//                        .email(request.getEmail())
+//                        .secret(passwordEncoder.encode(request.getPassword()))
+//                        .role(Role.User)
+//                        .build();
+//        repository.save(user);
+//        var jwtToken = jwtService.generateToken(user);
+//
+//        return AuthenticationResponse.builder().token(jwtToken).build();
+//    }
+//
+//    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+//        authenticationManager.authenticate(
+//                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+//        var user = repository.findByEmail(request.getEmail()).orElseThrow();
+//        var jwtToken = jwtService.generateToken(user);
+//
+//        return AuthenticationResponse.builder().token(jwtToken).build();
+//    }
+//}
